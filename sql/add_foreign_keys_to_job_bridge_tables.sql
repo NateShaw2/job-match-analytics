6c4208c96b16828809885802218b3825de8b33da
NateShaw2 <nshaw238@gmail.com>
2026-05-31 19:51:50 -0700

    Enforce consistent style patterns

--- sql/add_foreign_keys_to_job_bridge_tables.sql
+++ sql/add_foreign_keys_to_job_bridge_tables.sql
@@ -1,5 +1,5 @@
 ALTER TABLE job_title_words
-ADD CONSTRAINT FK_job_title_words FOREIGN KEY (job_id) REFERENCES Jobs(job_id);
+ADD CONSTRAINT FK_job_title_words FOREIGN KEY (job_id) REFERENCES jobs(job_id);
 
 ALTER TABLE job_query_keywords
-ADD CONSTRAINT FK_job_query_keywords FOREIGN KEY (job_id) REFERENCES Jobs(job_id);
+ADD CONSTRAINT FK_job_query_keywords FOREIGN KEY (job_id) REFERENCES jobs(job_id);
