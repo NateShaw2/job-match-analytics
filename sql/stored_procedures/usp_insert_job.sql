4e7373309ffb98a862dfcf58c1a219b9cd25624d
Nate <nshaw238@gmail.com>
2026-06-01 20:25:58 -0700

    Add job data to SQL Server
    The usp_insert_job stored procedure is responsible
    for inserting data to the job table and the bridge tables
    for the job query keywords and job title words views that
    will be created.

---
+++ sql/stored_procedures/usp_insert_job.sql
@@ -0,0 +1,106 @@
+-- =============================================
+-- Create date: 6/1/2026
+-- Description:	Inserts job data to their respective tables
+-- =============================================
+CREATE PROCEDURE usp_insert_job
+    @job_id VARCHAR(255),
+    @job_title VARCHAR(255) = NULL,
+    @employer_name VARCHAR(255) = NULL,
+    @employer_website VARCHAR(255) = NULL,
+    @job_publisher VARCHAR(255) = NULL,
+    @job_employment_type VARCHAR(255) = NULL,
+    @job_description VARCHAR(MAX),
+    @job_posted_at VARCHAR(255) = NULL,
+    @job_posted_at_datetime_utc DATETIME = NULL,
+    @job_rated_at DATETIME,
+    @job_location VARCHAR(255) = NULL,
+    @job_city VARCHAR(255) = NULL,
+    @job_state VARCHAR(255) = NULL,
+    @job_country VARCHAR(255) = NULL,
+    @job_benefits VARCHAR(MAX) = NULL,
+    @job_salary_string VARCHAR(255) = NULL,
+    @job_min_salary INT = NULL,
+    @job_max_salary INT = NULL,
+    @job_salary_period VARCHAR(255) = NULL,
+    @job_score TINYINT,
+    @job_score_reasoning VARCHAR(MAX),
+    @job_skills_required VARCHAR(MAX),
+    @job_posting_quality_score TINYINT,
+    @job_posting_quality_score_reasoning VARCHAR(MAX),
+    @job_query_search VARCHAR(255),
+    @resume_id INT,
+    @query_keywords VARCHAR(MAX),
+    @title_words VARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT 1 FROM jobs WHERE job_id = @job_id)
+    BEGIN
+        INSERT INTO jobs (
+            job_id,
+            job_title,
+            employer_name,
+            employer_website,
+            job_publisher,
+            job_employment_type,
+            job_description,
+            job_posted_at,
+            job_posted_at_datetime_utc,
+            job_rated_at,
+            job_location,
+            job_city,
+            job_state,
+            job_country,
+            job_benefits,
+            job_salary_string,
+            job_min_salary,
+            job_max_salary,
+            job_salary_period,
+            job_score,
+            job_score_reasoning,
+            job_skills_required,
+            job_posting_quality_score,
+            job_posting_quality_score_reasoning,
+            job_query_search,
+            resume_id
+        )
+        VALUES (
+            @job_id,
+            @job_title,
+            @employer_name,
+            @employer_website,
+            @job_publisher,
+            @job_employment_type,
+            @job_description,
+            @job_posted_at,
+            @job_posted_at_datetime_utc,
+            @job_rated_at,
+            @job_location,
+            @job_city,
+            @job_state,
+            @job_country,
+            @job_benefits,
+            @job_salary_string,
+            @job_min_salary,
+            @job_max_salary,
+            @job_salary_period,
+            @job_score,
+            @job_score_reasoning,
+            @job_skills_required,
+            @job_posting_quality_score,
+            @job_posting_quality_score_reasoning,
+            @job_query_search,
+            @resume_id
+        );
+
+        INSERT INTO job_query_keywords (job_id, job_query_keyword)
+        SELECT @job_id, TRIM(value)
+        FROM STRING_SPLIT(@query_keywords, ',');
+
+        INSERT INTO job_title_words (job_id, job_title_word)
+        SELECT @job_id, TRIM(value)
+        FROM STRING_SPLIT(@title_words, ',');
+    END
+END
+GO
