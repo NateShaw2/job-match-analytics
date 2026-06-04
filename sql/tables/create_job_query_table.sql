1b67b62d8af4eb7f374dd54d5b52c06b94bdcc34
Nate <nshaw238@gmail.com>
2026-06-04 15:53:12 -0700

    Rename table queries

--- sql/tables/create_job_query_table.sql
+++ sql/tables/create_job_query_table.sql
@@ -3,3 +3,6 @@ CREATE TABLE job_query (
 	job_query VARCHAR(255),
 	CONSTRAINT PK_job_query PRIMARY KEY(job_id, job_query)
 )
+
+ALTER TABLE job_query
+ADD CONSTRAINT FK_job_query FOREIGN KEY (job_id) REFERENCES jobs(job_id);
